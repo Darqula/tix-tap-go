d03b00aa245617a6dfa23ea7d0d2e67b8c6d2405
Darqula <25856730+darqula@users.noreply.github.com>
2026-08-16 23:11:26 +0500

    Added shared project

---
+++ src/backend/Shared/DAL/SharedDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Entities;
+
+namespace Shared.DAL;
+
+public abstract class SharedDbContext(DbContextOptions options) : DbContext(options)
+{
+    public override int SaveChanges()
+    {
+        ProcessTrackedEntities();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        ProcessTrackedEntities();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void ProcessTrackedEntities()
+    {
+        foreach (var changedEntry in ChangeTracker.Entries<EntityBase>())
+        {
+            switch (changedEntry.State)
+            {
+                case EntityState.Added:
+                    changedEntry.Entity.CreatedAt = DateTimeOffset.UtcNow;
+                    changedEntry.Entity.UpdatedAt = DateTimeOffset.UtcNow;
+                    break;
+                case EntityState.Modified:
+                    changedEntry.Entity.UpdatedAt = DateTimeOffset.UtcNow;
+                    break;
+                case EntityState.Deleted:
+                    changedEntry.Entity.IsDeleted = true;
+                    changedEntry.Entity.UpdatedAt = DateTimeOffset.UtcNow;
+                    changedEntry.State = EntityState.Modified;
+                    break;
+            }
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (entityType.ClrType.IsAssignableTo(typeof(EntityBase)))
+            {
+                var entityTypeBuilder = modelBuilder.Entity(entityType.ClrType);
+                entityTypeBuilder.Property<DateTimeOffset>(nameof(EntityBase.CreatedAt)).HasDefaultValueSql("now()");
+                entityTypeBuilder.Property<DateTimeOffset>(nameof(EntityBase.UpdatedAt)).HasDefaultValueSql("now()");
+                entityTypeBuilder.Property<bool>(nameof(EntityBase.IsDeleted)).HasDefaultValue(false);
+            }
+        }
+    }
+}
---
+++ src/backend/Shared/Entities/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Entities;
+
+public abstract class EntityBase
+{
+    public Guid Id { get; init; }
+    [Required]
+    public DateTimeOffset CreatedAt { get; internal set; }
+    [Required]
+    public DateTimeOffset UpdatedAt { get; internal set; }
+    [Required]
+    public bool IsDeleted { get; internal set; }
+}
